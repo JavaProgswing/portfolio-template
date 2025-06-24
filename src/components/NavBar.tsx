4e5883427fc3803fb9edeb6860dd517fbf385659
Yashasvi Allen <yashasviallen@gmail.com>
2025-06-24 23:29:35 +0530

    navbar contactbadges fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Spacer } from "@chakra-ui/react";
 import { Info } from "./Intro";
 import ContactBadges from "./ContactBadges";
 import ColorModeToggle from "./ColorModeToggle";
@@ -9,26 +9,24 @@ interface Props {
 
 const Navbar = ({ data }: Props) => {
   return (
-    <Box position="relative" w="100%" px={5} py={3}>
-      <HStack justifyContent="space-between" w="100%">
-        {/* Left */}
-        <Heading
-          as="h1"
-          size="md"
-          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        >
-          ~
-        </Heading>
+    <HStack w="100%" px={5} py={3}>
+      {/* Left Item */}
+      <Heading
+        as="h1"
+        size="md"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        ~
+      </Heading>
 
-        {/* Right */}
-        <ColorModeToggle />
-      </HStack>
-
-      {/* Center - absolute, overlaid in the middle */}
-      <Box position="absolute" left="50%" transform="translateX(-50%)">
+      {/* Center Item (ContactBadges) */}
+      <Box flex="1" display="flex" justifyContent="center">
         <ContactBadges contacts={data.contacts} />
       </Box>
-    </Box>
+
+      {/* Right Item */}
+      <ColorModeToggle />
+    </HStack>
   );
 };
 
